179975ada1ef8c23ddbb57994e5123a4c8247486
Allan D <afdeltour@gmail.com>
2023-03-26 19:45:34 +0200

    FIX Typo

--- src/features/auth/AuthSlice.jsx
+++ src/features/auth/AuthSlice.jsx
@@ -8,7 +8,7 @@ const initialState = {
   success: false,
 }
 
-const authSlice = createSlice({
+export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
--- src/store/Store.jsx
+++ src/store/Store.jsx
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice'
+import authReducer from '../features/auth/AuthSlice'
 
 export const Store = configureStore({
   reducer: {

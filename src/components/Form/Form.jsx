3a88c1f256d58e33149934e484ae80616fea8293
Allan D <afdeltour@gmail.com>
2023-03-28 22:36:02 +0200

    ADD Swagger files FEAT Add Error for wrong IDs

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -6,6 +6,7 @@ import { loadData, saveData } from "../../services/LocalStorage";
 import './Form.css'
 
 export const Form = () => {
+    const [error, setError] = useState(false)
     const [login, setLogin] = useState({
         email: "",
         password: "",
@@ -30,7 +31,10 @@ export const Form = () => {
     const handleSubmit = async e => {
         e.preventDefault();
         const fetchedData = await FetchData('user/login', 'POST', {body:login})
-        await dispatch({type:"auth/addUserToken", payload: fetchedData.body.token})
+        if (fetchedData.status === 400) {
+            setError(true)
+        }
+        dispatch({type:"auth/addUserToken", payload: fetchedData.body.token})
         saveData('token', fetchedData.body.token)
         navigate('/profile');
     }
@@ -43,16 +47,17 @@ export const Form = () => {
                 <form id="form" onSubmit={handleSubmit}>
                     <div className="input-wrapper">
                         <label htmlFor="username"> Username </label>
-                        <input type="text" value={email} name="email" id="username" onChange={handleChange}/>
+                        <input type="text" value={email} name="email" id="username" placeholder="steve@rogers.com" onChange={handleChange}/>
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password"> Password </label>
-                        <input type="password" value={password} name="password" id="password" onChange={handleChange}/>
+                        <input type="password" value={password} name="password" id="password" placeholder="password456" onChange={handleChange}/>
                     </div>
                     <div className="input-remember">
                         <input type="checkbox" id="remember-me"/>
                         <label htmlFor="remember-me"> Remember me </label>
                     </div>
+                    <div className={error ? "error__message" : "error__message__closed"} > Wrong ID </div>
                     <button className="sign-in-button"> Sign in </button>
                 </form>
             </section>
